fd1e7bab06df0dc72d55171dbf3b79ccd52ae6a2
Maksim Lin <maks@manichord.com>
2026-05-11 16:18:20 +1000

    more layout fixes

--- static-app/script.js
+++ static-app/script.js
@@ -191,27 +191,6 @@ window.onload = () => {
   for (const k in defaultColors) {
     up(k, defaultColors[k]);
   }
-  // Populate preview monitor with tracker rows (static replica of original Jinja output)
-  const TRACKER_ROWS = ["00","01","02","03","04","05","06","07","08","09","0A","0B","0C","0D","0E","0F"];
-  let previewHTML = `<span class="fg">song sad-fog</span>                         <span class="info">[c++f]</span>`;
-  TRACKER_ROWS.forEach((row, idx) => {
-    if (idx === 0) {
-      previewHTML += `\n<span class="hi1">${row}</span>  <span class="cursor">00</span>`;
-    } else {
-      previewHTML += `\n<span class="hi1">${row}</span>  <span class="fg">--</span>`;
-    }
-    // add six placeholder '--' spans
-    for (let i = 0; i < 6; i++) {
-      previewHTML += `  <span class="fg">--</span>`;
-    }
-  });
-  previewHTML += `\n<span class="fg"></span>`;
-
-  previewHTML += `\n<span class="fg"><span class="hi1">D</span></span>`;
-  previewHTML += `\n<span class="fg"><span class="hi1">P</span><span class="fg"> G</span></span>`;
-  previewHTML += `\n<span class="fg"><span class="hi2">S</span><span class="fg">CPI</span></span>`;
-  previewHTML += `\n<span class="fg"><span class="hi2">M</span><span class="fg"> T T</span></span>`;
-  document.getElementById('screen-song').innerHTML = previewHTML;
   buildColorInputs();
   renderG();
 };
